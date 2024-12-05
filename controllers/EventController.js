8b9c22e3efa47dff849f1619e1029bcc946f6518
Erick John Orqueza Lopez <112525487+CyberWhisker@users.noreply.github.com>
2024-12-05 17:36:38 +0800

    update for Request

--- controllers/EventController.js
+++ controllers/EventController.js
@@ -17,7 +17,7 @@ const getDataById = async (req, res) => {
         return res.status(404).json({ error: 'Not valid ID' })
     }
     try {
-        const data = await Model.findById(id).sort({ createdAt: -1 })
+        const data = await Model.findOne({ _id: id }).sort({ createdAt: -1 })
         res.status(200).json(data)
     } catch (error) {
         res.status(400).json({ error: error.message })
